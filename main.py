ec7e4dcf13d84081a6d0861b972185c0ba10c865
Perevoshikov_89 <yuri.perevoshikov@gmail.com>
2026-05-04 19:19:42 +0400

    правки

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# POST-запросы КО в XML по маске файлов с постновкой ЭЦП, получение КО, снятие ЭЦП, форматирование ответа
 import requests
 import subprocess as sp
 import os
@@ -6,77 +5,134 @@ import vkbeautify as vkb
 import shlex
 import fnmatch
 
-NBKIpath: str = r"D:\POST\НБКИ\Автозапросы"
+NBKIpath = r"D:\POST\НБКИ\Автозапросы"
 os.chdir(NBKIpath)
 
-#reqName = "req_UL_ЛОСИНАЯ_BHIP_mf0_v29.xml"
-#reqName = 'req_FL_КАРТОЧНЫЙ_CDSC_mf4_v29.xml'
-reqName = 'req_*.xml'
-
+reqName = "req_*.xml"
 host = 100
 
-sps = fnmatch.filter([fls for fls in os.listdir(NBKIpath)], reqName)
+# ---------------- URL ----------------
+if host == 100:
+    url = "http://client.demo.nbki.msk:8082/products/B2BRUTDF"
+elif host == 102:
+    url = "http://client-alfa.demo.nbki.msk:8082/products/B2BRUTDF"
+elif host == 103:
+    url = "https://reports.test-alfa.nbki.ru/products/B2BRUTDF"
+elif host == 200:
+    url = "http://client.demo.nbki.msk:8082/NbchScore"
+else:
+    raise ValueError(f"Хост неизвестен: {host}")
+
+# ---------------- файлы ----------------
+sps = fnmatch.filter(os.listdir(NBKIpath), reqName)
 count = 0
 
-if host == 100:
-    url = "http://client.demo.nbki.msk:8082/products/B2BRUTDF"           # клиентский сервер 100
-elif host == 102:
- url = "http://client-alfa.demo.nbki.msk:8082/products/B2BRUTDF"       # тестовый  102
-elif host == 103:
-    url = "https://reports.test-alfa.nbki.ru/products/B2BRUTDF"  # не используется
-elif host == 200:
-        url = "http://client.demo.nbki.msk:8082/NbchScore"  # запросы ПКР ???
-else:
- print ("Хост неизвестен:", host)
- #url = "http://ru-icrs-app-2.demo.nbki.msk:8082/products/B2BRequestServlet"   # неизвестно кто
- exit(0)
-
 for reqFile in sps:
-    print("Исходный файл", reqFile, "найден.")
+    print(f"\nОбработка файла: {reqFile}")
 
-    # форматирование исходного файла запроса, сдвиги секций согласно вложениям
-    fi = open(reqFile, "r")
-    fo = open("s" + reqFile, "w")
-    datareq = fi.read()
-    fo.write(vkb.xml(datareq))
-    fo.close()
-    fi.close()
-    os.remove(reqFile)
-    os.rename("s" + reqFile, reqFile)
+    # -------- форматирование XML --------
+    try:
+        with open(reqFile, "r", encoding="utf-8") as fi:
+            datareq = fi.read()
 
-    # Ставим ЭЦП на текстовый файл запроса
-    yesf = open("cp_yes.baton", "r")
-    CREATE_NO_WINDOW = 0x08000000
-    args = shlex.split('ecp.bat ' + reqFile)
-    result = sp.run(args, stdout=sp.PIPE, stdin=yesf, creationflags=CREATE_NO_WINDOW)
-    yesf.close()
-    print("Файл подписан ЭЦП. Подпись:", args[0])
+        formatted = vkb.xml(datareq)
 
-    f = {'file': ('file', open(reqFile + ".sig", 'rb'))}
+        temp_name = "s_" + reqFile
+        with open(temp_name, "w", encoding="utf-8") as fo:
+            fo.write(formatted)
 
-    out = open("Pyt.xml", "wb")
-    response = requests.post(url, data=f['file'][1].read(), headers={"Content-Type": "application/pkcs7"})
-    print("Ответ хоста получен.")
-    out.write(response.content)
-    out.close()
-    f['file'][1].close()
+        os.remove(reqFile)
+        os.rename(temp_name, reqFile)
 
-    # Снимаем ЭЦП в файле ответа
-    yesf = open("cp_yes.baton", "r")
-    CREATE_NO_WINDOW = 0x08000000
-    args = shlex.split('cp.bat Pyt.xml')
-    result = sp.run(args, stdout=sp.PIPE, stdin=yesf, creationflags=CREATE_NO_WINDOW)
-    yesf.close()
-    print("ЭЦП снята.")
+    except Exception as e:
+        print(f"Ошибка форматирования XML: {e}")
+        continue
 
-    ind = open('des_Pyt.xml', 'r')
-    out = open(str.replace(reqFile, 'req_', 'KO_'), 'w')
-    out.write(vkb.xml(ind.read()))
-    print("Результат записан в ", out.name)
-    out.close()
-    ind.close()
-    os.remove(reqFile + '.sig')
-    os.remove('des_Pyt.xml')
-    os.remove('Pyt.xml')
+    # -------- ЭЦП подпись --------
+    try:
+        yesf = open("cp_yes.baton", "r")
+
+        CREATE_NO_WINDOW = 0x08000000
+        args = shlex.split(f'ecp.bat {reqFile}')
+
+        result = sp.run(
+            args,
+            stdin=yesf,
+            stdout=sp.PIPE,
+            creationflags=CREATE_NO_WINDOW
+        )
+
+        yesf.close()
+        print("Файл подписан ЭЦП")
+
+    except Exception as e:
+        print(f"Ошибка подписи ЭЦП: {e}")
+        continue
+
+    # -------- отправка --------
+    sig_file = reqFile + ".sig"
+
+    try:
+        with open(sig_file, "rb") as f:
+            response = requests.post(
+                url,
+                data=f.read(),
+                headers={"Content-Type": "application/pkcs7"}
+            )
+
+        print("Ответ хоста получен")
+
+        with open("Pyt.xml", "wb") as out:
+            out.write(response.content)
+
+    except Exception as e:
+        print(f"Ошибка запроса: {e}")
+        continue
+
+    # -------- снятие ЭЦП --------
+    try:
+        yesf = open("cp_yes.baton", "r")
+
+        CREATE_NO_WINDOW = 0x08000000
+        args = shlex.split("cp.bat Pyt.xml")
+
+        result = sp.run(
+            args,
+            stdin=yesf,
+            stdout=sp.PIPE,
+            creationflags=CREATE_NO_WINDOW
+        )
+
+        yesf.close()
+        print("ЭЦП снята")
+
+    except Exception as e:
+        print(f"Ошибка снятия ЭЦП: {e}")
+        continue
+
+    # -------- сохранение результата --------
+    try:
+        out_name = reqFile.replace("req_", "KO_")
+
+        with open("des_Pyt.xml", "r", encoding="utf-8") as ind:
+            result_xml = vkb.xml(ind.read())
+
+        with open(out_name, "w", encoding="utf-8") as out:
+            out.write(result_xml)
+
+        print(f"Результат записан в {out_name}")
+
+    except Exception as e:
+        print(f"Ошибка записи результата: {e}")
+
+    # -------- чистка --------
+    try:
+        os.remove(sig_file)
+        os.remove("des_Pyt.xml")
+        os.remove("Pyt.xml")
+    except:
+        pass
+
     count += 1
-print('Сделано', count)
+
+print(f"\nГотово. Обработано файлов: {count}")
